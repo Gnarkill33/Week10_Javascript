b185427c14dcbf9d3364d98ce702b1e47dd05cc7
Gnarkill33 <gnarkill33@gmail.com>
2023-10-15 22:00:05 +0300

    Extra Task

---
+++ Extra Task/assets/scripts/index.js
@@ -0,0 +1,19 @@
+
+
+//1. С помощью alert
+
+/*function calculateTotalPrice(quantity = "2", price = "15000000") {
+    const totalPrice = quantity * price;
+    const totalPriceRub = (totalPrice.toLocaleString("ru-RU"));
+    alert ("Стоимость покупки: " + totalPriceRub + " рублей");
+}*/    
+
+//2. Вывод сообщения
+
+/*function calculateTotalPrice(quantity = "2", price = "15000000") {
+    const totalPrice = quantity * price;
+    const totalPriceRub = (totalPrice.toLocaleString("ru-RU"));
+    let message = 'СТОИМОСТЬ ПОКУПКИ: ' + totalPriceRub + ' РУБЛЕЙ';
+    const payment = document.getElementById('total');
+    payment.textContent = (message);
+}*/    
